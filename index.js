b7be81996261f01ee37e59a36403a415eb36d6e7
Malvin King <malvintech2@gmail.com>
2025-07-07 13:02:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star MALVIN-XD 
+Don't Forgot To Give Star MALVIN-XD
 
  Server running on http://localhost:` + PORT)
 })
